aaf3b3e99acd140d7c9c08a5c179383f667beb85
starwarriorman <140275760+starwarriorman@users.noreply.github.com>
2023-07-22 18:05:00 -0700

    Add files via upload

---
+++ example.java
@@ -0,0 +1,13 @@
+public class example {
+    public static void main(String[] args) {
+      int a = 5;
+      int b = 6;
+      int c = 7;
+      int x = a + b;
+      System.out.println(" a + b ");
+      System.out.println(" a " + " b ");
+      System.out.println( a + b ) ;
+      System.out.println( " a " + b);
+      System.out.println( a + "b");
+      System.out.println ( " a + b = " + x);
+  }}
---
+++ javascript.js
@@ -0,0 +1,24 @@
+// How to create variables:
+var x;
+let y;
+
+// How to use variables:
+x = 5;
+y = 6;
+let z = x + y;
+
+function appendString()
+    {
+        var math;
+        math = 1 + 2;
+        math++
+        document.getElementById("TestParagraph").innerHTML = math;
+    }
+function appendString2()
+    {
+        var math;
+        math = 11 +6;
+        document.getElementById("TestParagraph").innerHTML = math;
+    }
+
+document.getElementById("math").innerHTML = z
